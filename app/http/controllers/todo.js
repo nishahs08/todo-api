4d807c39aaf004fd3069bcc636544e9b512ec3b8
Nisha Singh <nishahs08@outlook.in>
2017-07-20 20:23:22 +0530

    todo update and todo delete added

--- app/http/controllers/todo.js
+++ app/http/controllers/todo.js
@@ -40,10 +40,45 @@ module.exports = {
     },
 
     update(req, res) {
-        
+        User.findById(req.params.body)
+        .then(user =>{
+          user.todos.foreach( function(todo){
+                if(todo.title === req.params.title){
+                    todo.description =req.body.description;
+                    todo.archived=req.body.archived;
+                    todo.status =req.body.status;
+                }
+          } )
+                return user.save();
+           }).then(user => res.json({
+                data: user,
+                success: true,
+                message: 'User created'
+            })).catch(err => res.status(500).json({
+                error: {
+                    code: err.code || 'E_SERVER_ERROR',
+                    message: err.message || 'Something bad happened'
+                },
+                success: false
+            }))
     },
 
     delete (req, res) {
-
+        User.findById(req.params.id)
+        .then(user =>{
+            user.todos.foreach(function(todo){
+                 if(todo.title === req.params.title){
+                     user.todos.splice(todo,1);
+            }
+            
+            return user.save();
+        })})
+        .catch(err => res.status(500).json({
+                error: {
+                    code: err.code || 'E_SERVER_ERROR',
+                    message: err.message || 'Something bad happened'
+                },
+                success: false
+            }))
     }
 }
