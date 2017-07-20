e9b3d45de1c7c7cff75e56f5521137393510bfe8
Nisha Singh <nishahs08@outlook.in>
2017-07-20 14:47:58 +0530

    Done with changes - Nice work

--- app/http/controllers/todo.js
+++ app/http/controllers/todo.js
@@ -20,15 +20,23 @@ module.exports = {
             }))
     },
 
-    create(req, res){
+    create(req, res) {
         User.findByIdAndUpdate(
-        req.params.userId,
-        {$push: {"todos": {title: req.body.title, description : req.body.description ,status :req.body.status,archived:req.body.archived}}},
-        {safe: true, upsert: true, new : true})
+            req.params.userId,
+            {
+                $push: {
+                    "todos": {
+                        title: req.body.title, 
+                        description : req.body.description
+                    }
+                }
+            },
+            { safe: true, upsert: true, new : true }
+        )
         .then(user => res.json({
                 data: user,
                 success: true,
-                message: 'User created'
+                message: 'Todo added'
             }))
         .catch(err => res.status(500).json({
                 error: {
